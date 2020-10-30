1072eda91bed8a7a1e71ff7145ef9da47a5c002a
GanatraJay2000 <jbg@somaiya.edu>
2020-10-30 20:35:58 +0530

    images links

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ wp_head();
 <body>
     <nav class="navbar navbar-expand-lg navbar-light pr-3 pl-5">
         <a class="navbar-brand" href="<?php echo site_url('/'); ?>">
-            <img src="https://josh.mcubeinfosys.com/wp-content/uploads/2020/10/josh-logo.png" alt="JOSH Logo" loading="lazy">Josh Foundation
+            <img src="./wp-content/uploads/2020/10/josh-logo.png" alt="JOSH Logo" loading="lazy">Josh Foundation
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
