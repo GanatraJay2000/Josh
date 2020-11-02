4133c2cdd2eefc4f7f8f15289c7ecafcb4e53259
GanatraJay2000 <jbg@somaiya.edu>
2020-11-02 19:48:59 +0530

    profile page

---
+++ page-profile.php
@@ -0,0 +1,100 @@
+<?php get_header(); ?>
+<div class="page-banner">
+    <div class="page-banner-info">
+        <h1 class="page-banner-heading">
+            About Us
+        </h1>
+        <h5 class="page-banner-sub-heading">
+            Profile
+        </h5>
+    </div>
+    <i class="fa fa-angle-down down-icon not-on-phone animate__animated animate__heartBeat animate__repeat-3 animate__slower animate__delay-2s"></i>
+</div>
+<div class="breadcrumbs not-on-phone">
+    <a href="<?php echo site_url('home'); ?>">Home</a>
+    <p>About Us - Profile</p>
+</div>
+<div>
+    <div class="heading px-4 px-md-5 pb-4 pb-md-5 pt-md-0 pt-4">
+        <h1 class="display-2 not-on-phone josh-page-title">JOSH</h1>
+        <p>
+            JOSH is a movement which was initiated by<br class="not-on-phone"> eminent ENT surgeon <span class="red">Dr. Jayant Gandhi</span> and <span class="red">Devangi Dalal</span> a renowned audiologist.
+        </p>
+    </div>
+    <div class="row d-flex flex-md-row flex-column">
+        <div class="col-lg-6 row-image"></div>
+        <div class="col-lg-6 row-info">
+            <p>
+                The movement is focused on supporting hearing impaired children, and inspired by the fact that scientific studies reveal that more than 95% of theses handicapped children have some residual hearing, which can be improved with the state-of-the-art medical technologies coupled with rehabilitation program.
+            </p>
+            <p>
+                With proper care and support all such children can, over a period of time, begin to hear well enough to respond to the spoken word and better integrate with society.
+            </p>
+            <p>
+                JOSH has been working towards this noble goal over the last 14 years.
+            </p>
+            <p>
+                Supported by philantropic donors, NGOs and manufacturers of hearing aids JOSH has been able to provide digital hearing aids to more than 1000+ underprivileged hearing impaired children-while establishing six special schools that provide education in a totally digital format.
+            </p>
+        </div>
+    </div>
+    <div class="row d-flex flex-md-row-reverse flex-column">
+        <div class="col-lg-6 row-image"></div>
+        <div class="col-lg-6 row-info">
+            <p>
+                This apart, JOSH has also organised various functions which honour a "JOSH Star" among the hearing impaired, thus infusing in these children a sense of well-being and self-confidence, while providing the motivation for others to overcome their handicap and do their parents proud.
+            </p>
+            <p>
+                Lend the deaf a ear. Be a part of <span class="red">MISSION JOSH</span> , and make a difference to their lives.
+            </p>
+        </div>
+    </div>
+    <div class="row d-flex flex-md-row flex-column">
+        <div class="col-lg-6 row-image"></div>
+        <div class="col-lg-6 row-info">
+            <p>
+                Keeping the above points in mind, we need to come to the main point and that is the course of action for achieving the goal that we have set before us. The most important steps are enlisted below:
+            </p>
+            <ol>
+                <li>
+                    Create awareness about need of integration of hearing impaired children in normal society.
+                </li>
+                <li>
+                    Impose ban on calling these special schools as "Deaf and Dumb School".
+                </li>
+                <li>
+                    These children are hearing impaired, but not dumb. They are as smart as any normal child.
+                </li>
+                <li>
+                    Group of NGOs can start adopting these special schools and try to provide them financial help to go for tailor-made digital hearing aids, creation of sound proof rooms, etc.
+                </li>
+                <li>
+                    Government can support these schools by dispensing proper hearing aids for these children.
+                </li>
+                <li>
+                    Large level awareness and appeal to donors are other important areas to help these children make their future better.
+                </li>
+            </ol>
+        </div>
+    </div>
+    <div class="row d-flex flex-md-row-reverse flex-column">
+        <div class="col-lg-6 row-image"></div>
+        <div class="col-lg-6 row-info">
+            <h1 class="red">MISSION</h1>
+            <p>
+                The movement is focused on supporting hearing impaired children, which can be improved with the state-of-the-art medical technologies coupled with rehabilitation program. With proper care and support all such children can, over a preiod of time, begin to hear well enough to respond to the spoken word and better integrate with society.
+            </p>
+        </div>
+    </div>
+    <div class="row d-flex flex-md-row flex-column">
+        <div class="col-lg-6 row-image"></div>
+        <div class="col-lg-6 row-info">
+            <h1 class="red">VISION</h1>
+            <p>
+                <i style="font-family:'PT Serif Bold'">"So near and yet so far".</i> This idiom aptly describes the state of affairs related to the rehabilitation of the hearing impaired persons in India. JOSH, dedicated to the task of bringing hearing impaired into the mainstream of the society, wonders why such an easy task is not being accomplished.
+            </p>
+
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
