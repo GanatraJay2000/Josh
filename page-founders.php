c8bfd0558896d3878f39e1cd09a21a3ecaeca3a6
GanatraJay2000 <jbg@somaiya.edu>
2020-11-03 15:31:55 +0530

    page founders

--- page-founders.php
+++ page-founders.php
@@ -14,4 +14,126 @@
     <a href="<?php echo site_url('home'); ?>">Home</a>
     <p>About Us - Founders</p>
 </div>
+<div class="profile-cards card-one">
+    <div class="d-flex flex-md-row flex-column">
+        <div class="text-center">
+            <img class="profile-photo" src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/doctor-jayant-gandhi.jpg" alt="">
+        </div>
+        <div class="mx-md-4 profile-info-container">
+            <ul class="profile-info nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="pills-one-info-tab" data-toggle="pill" href="#pills-one-info" role="tab" aria-controls="pills-one-info" aria-selected="true">Info</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-one-publications-tab" data-toggle="pill" href="#pills-one-publications" role="tab" aria-controls="pills-one-publications" aria-selected="false">Publications</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-one-achievements-tab" data-toggle="pill" href="#pills-one-achievements" role="tab" aria-controls="pills-one-achievements" aria-selected="false">Achievements</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-one-more-tab" data-toggle="pill" href="#pills-one-more" role="tab" aria-controls="pills-one-more" aria-selected="false">More</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-one-info" role="tabpanel" aria-labelledby="pills-info-tab">
+                    <u>Education</u>
+                    <p>M.S. (ENT),D.O.R.L.,F.C.P.S .</p>
+                    <u>Clinic</u>
+                    <p>Gandhi Nursing Home Santacruz (WEST).</p>
+                    <u>Professional Experience</u>
+                    <p>Consulting ENT surgeon since 30 years.</p>
+                </div>
+                <div class="tab-pane fade" id="pills-one-publications" role="tabpanel" aria-labelledby="pills-one-publications-tab">
+                    <u>Publication:</u>
+                    <p>
+                        They have published and Presented articles in National and International conference written articles in Times of India.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-one-achievements" role="tabpanel" aria-labelledby="pills-one-achievements-tab">
+                    <u> Social Achievements:</u>
+                    <p>
+                        Joint Secretary - Shri Vile Parle Kelavani Mandal-Largest Educational trust-Mumbai.<br>
+                        Chairman - SVKM International school.<br>
+                        Mentor - SVKM IB School.<br>
+                        President - Lions Club of Juhu (A premier club of India).<br>
+                        Founder trustee - JOSH (Juvenile Organization of Speech and Hearing impaired.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-one-more" role="tabpanel" aria-labelledby="pills-one-more-tab">
+                    <u>Awards Achieved:</u>
+                    <p>
+                        Gold medal in M. S. (ENT) Mumbai University.<br>
+                        Recipient of Best Hearing Aid center award.
+                    </p>
+                    <u>International Training:</u>
+                    <p>
+                        Micro Ear surgery in West Germany.<br>
+                        Nasal endoscopy surgery in Austria.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="profile-cards card-two">
+    <div class="d-flex flex-md-row flex-column">
+        <div class="text-center">
+            <img class="profile-photo" src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/doctor-jayant-gandhi.jpg" alt="">
+        </div>
+        <div class="mx-md-4 profile-info-container">
+            <ul class="profile-info nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="pills-two-info-tab" data-toggle="pill" href="#pills-two-info" role="tab" aria-controls="pills-two-info" aria-selected="true">Info</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-two-publications-tab" data-toggle="pill" href="#pills-two-publications" role="tab" aria-controls="pills-two-publications" aria-selected="false">Publications</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-two-achievements-tab" data-toggle="pill" href="#pills-two-achievements" role="tab" aria-controls="pills-two-achievements" aria-selected="false">Achievements</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="pills-two-more-tab" data-toggle="pill" href="#pills-two-more" role="tab" aria-controls="pills-two-more" aria-selected="false">More</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-two-info" role="tabpanel" aria-labelledby="pills-two-info-tab">
+                    <u>Education</u>
+                    <p>M.S. (ENT),D.O.R.L.,F.C.P.S .</p>
+                    <u>Clinic</u>
+                    <p>Gandhi Nursing Home Santacruz (WEST).</p>
+                    <u>Professional Experience</u>
+                    <p>Consulting ENT surgeon since 30 years.</p>
+                </div>
+                <div class="tab-pane fade" id="pills-two-publications" role="tabpanel" aria-labelledby="pills-two-publications-tab">
+                    <u>Publication:</u>
+                    <p>
+                        They have published and Presented articles in National and International conference written articles in Times of India.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-two-achievements" role="tabpanel" aria-labelledby="pills-two-achievements-tab">
+                    <u> Social Achievements:</u>
+                    <p>
+                        Joint Secretary - Shri Vile Parle Kelavani Mandal-Largest Educational trust-Mumbai.<br>
+                        Chairman - SVKM International school.<br>
+                        Mentor - SVKM IB School.<br>
+                        President - Lions Club of Juhu (A premier club of India).<br>
+                        Founder trustee - JOSH (Juvenile Organization of Speech and Hearing impaired.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-two-more" role="tabpanel" aria-labelledby="pills-two-more-tab">
+                    <u>Awards Achieved:</u>
+                    <p>
+                        Gold medal in M. S. (ENT) Mumbai University.<br>
+                        Recipient of Best Hearing Aid center award.
+                    </p>
+                    <u>International Training:</u>
+                    <p>
+                        Micro Ear surgery in West Germany.<br>
+                        Nasal endoscopy surgery in Austria.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
