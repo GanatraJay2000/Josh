6f2b608792e56c39f60bf28fd10d1478ee9254bc
GanatraJay2000 <jbg@somaiya.edu>
2020-11-03 11:12:10 +0530

    changes home,founders

--- front-page.php
+++ front-page.php
@@ -21,19 +21,27 @@
     </div>
 </div>
 <div class="my-4">
-    <div class="heading px-4 mb-5">
-        <h2 class="mb-3 text-md-center"><b>Welcome to Josh</b></h2>
+    <div class="heading">
+        <h2 class="mb-5 text-md-center">Welcome to Josh</h2>
         <p>JOSH is a movement founded by eminent ENT surgeon Dr.Jayant Gandhi and Devangi Dalal, a renowned audiologist. We focus on supporting hearing impaired children, with the state-of-the-art medical technologies coupled with rehabilitation program. With proper care and support all such children can, over a peroid of time, begin to gear well enough to respond to the spoken word and better integrate with society.</p>
     </div>
     <div class="our-activities mb-5">
         <img class="hearing-aid" src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/activity_josh.jpg" alt="Hearing Aid">
         <div class="p-4">
             <h2 class="mb-3 text-xs-center text-md-left">Our Activities</h2>
-            <p>Create awareness about need of integration of hearing impaired children in normal society.<br><br>
+            <p>Create awareness about need of integration of hearing impaired children in normal society.
+            </p>
+            <p>
                 Impose ban on calling these special schools as 'Deaf and Dumb School.
-                These Children are hearing impaired, but not dumb. They are as smart as any normal child.<br><br>
-                Group of NGOs can start adopting these special schools and try to provide them financial help to go for tailor-made digital hearing aids, creation of sound proof rooms, etc.<br><br>
-                Goverment can support these schools by dispensing proper hearing aids for these children.<br><br>
+                These Children are hearing impaired, but not dumb. They are as smart as any normal child.
+            </p>
+            <p>
+                Group of NGOs can start adopting these special schools and try to provide them financial help to go for tailor-made digital hearing aids, creation of sound proof rooms, etc.
+            </p>
+            <p>
+                Goverment can support these schools by dispensing proper hearing aids for these children.
+            </p>
+            <p>
                 Large level awarness and appeal to donors are other important areas to help these children make their future better.
             </p>
             <button class="red-btn">Learn More</button>
@@ -58,13 +66,13 @@
         </div>
     </div>
     <div class="latest-blogs-container">
-        <h1 class="text-center mb-4 mb-md-5">Latest Blogs</h1>
+        <h1 class="display-4 text-center mb-5">Latest Blogs</h1>
         <div class="latest-blogs">
             <div class="blog mx-0 mx-md-5 mb-xs-4 mb-md-0">
                 <img class="blog-image" src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/blog_josh_2-scaled.jpg" alt="Blog Banner">
                 <div class="blog-info">
                     <div class="date">24.10.2020</div>
-                    <h3 class="mb-2">Lorem Ipsum Dolor</h3>
+                    <h3>Lorem Ipsum Dolor</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, non. Numquam quos magni tenetur!</p>
                     <button class="red-btn">Continue Reading</button>
                 </div>
@@ -73,7 +81,7 @@
                 <img class="blog-image" src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/blog_josh_2-scaled.jpg" alt="Blog Banner">
                 <div class="blog-info">
                     <div class="date">24.10.2020</div>
-                    <h3 class="mb-2">Lorem Ipsum Dolor</h3>
+                    <h3>Lorem Ipsum Dolor</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, non. Numquam quos magni tenetur!</p>
                     <button class="red-btn">Continue Reading</button>
                 </div>
