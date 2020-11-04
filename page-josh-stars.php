c6f8f0ed956e7b112bbe048f8e07205b5d42ac7c
Jay Ganatra <jbg@somaiya.edu>
2020-11-04 11:14:13 +0530

    page josh-stars

--- page-josh-stars.php
+++ page-josh-stars.php
@@ -14,4 +14,20 @@
     <a href="<?php echo site_url('home'); ?>">Home</a>
     <p>Our Activities - Josh Stars</p>
 </div>
+<div class="stars-cards">
+    <div class="star-card">
+        <a href="#" class="star-card-heading red-a">Josh Foundation Dance Competetion Media Coverage</a>
+        <small class="star-card-date">10th Dec 2016 - 2nd Jan 2017</small>
+        <a href="#" class="view-pdf">
+            <i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp; View PDF
+        </a>
+    </div>
+    <div class="star-card">
+    <a href="#" class="star-card-heading red-a">Hearing Impairment to Empowerment</a>
+        <small class="star-card-date">Josh Foundation</small>
+        <a href="#" class="view-pdf">
+            <i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp; View PDF
+        </a>
+    </div>
+</div>
 <?php get_footer(); ?>
