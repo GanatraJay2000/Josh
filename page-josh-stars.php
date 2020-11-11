4540ea5c71a7d05b429d1ceaf543d8dfac3d617c
GanatraJay2000 <jbg@somaiya.edu>
2020-11-11 16:37:31 +0530

    changes josh-stars, press

--- page-josh-stars.php
+++ page-josh-stars.php
@@ -23,7 +23,7 @@ $pdfs = rwmb_meta('pdf');
     <?php
     foreach($pdfs as $key=>$pdf){ $pdf = array_values($pdf); ?>
     <div class="star-card">
-        <a href="#" class="star-card-heading red-a"><?php print_r($titles[$key]); ?></a>
+        <a href="<?php print_r($pdf[0]['url']); ?>" target="_blank" class="star-card-heading red-a"><?php print_r($titles[$key]); ?></a>
         <small class="star-card-date"><?php print_r($dates[$key]);   ?></small>
         <a href="<?php print_r($pdf[0]['url']); ?>" target="_blank" class="view-pdf">
             <i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp; View PDF
--- page-press.php
+++ page-press.php
@@ -11,14 +11,21 @@
     <a href="<?php echo site_url('home'); ?>">Home</a>
     <p>Press</p>
 </div>
+<?php
+$titles = rwmb_meta('title');
+$dates = rwmb_meta('date');
+$pdfs = rwmb_meta('pdf');
+?>
 <div class="press-cards">
+    <?php
+    foreach($pdfs as $key=>$pdf){ $pdf = array_values($pdf); ?>
     <div class="press-card">
-        <a href="#" class="press-card-heading red-a">Remo D’souza joins hands as brand ambassador with 
-        JOSH FOUNDATION</a>
-        <small class="press-card-date">10th Dec 2016 - 2nd Jan 2017</small>
-        <a href="#" class="view-pdf">
+        <a href="<?php print_r($pdf[0]['url']); ?>" target="_blank" class="press-card-heading red-a"><?php print_r($titles[$key]); ?></a>
+        <small class="press-card-date"><?php print_r($dates[$key]);   ?></small>
+        <a href="<?php print_r($pdf[0]['url']); ?>" target="_blank" class="view-pdf">
             <i class="fa fa-file-pdf-o" aria-hidden="true"></i>&nbsp;&nbsp; View PDF
         </a>
-    </div>
+    </div>  
+    <?php } ?>  
 </div>
 <?php get_footer(); ?>
