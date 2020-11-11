6344b41f99dba85b960e19f2b5cd94e664439c2d
GanatraJay2000 <jbg@somaiya.edu>
2020-11-11 15:24:46 +0530

    changes header-footer

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ wp_head();
 
     <nav class="navbar navbar-expand-lg navbar-light pr-lg-3 pl-lg-5">
         <a class="navbar-brand" href="<?php echo site_url('/'); ?>">
-            <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/josh-logo.png" alt="JOSH Logo" loading="lazy">Josh Foundation
+            <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/josh-logo.png" alt="JOSH Logo" loading="lazy">Josh Foundation
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controsls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fa fa-bars navbar-toggler-my- icon"></i>
