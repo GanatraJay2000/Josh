3b781ca35ebf270e41f48a9ed0ca15cefb61bfe7
GanatraJay2000 <jbg@somaiya.edu>
2020-11-11 15:23:06 +0530

    changes front page

--- front-page.php
+++ front-page.php
@@ -2,19 +2,19 @@
 
 <div class="banner slideshow">
     <div class="slideshow-item">
-        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/josh_b1.jpg" alt="banner_1">
+        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/josh_b1.jpg" alt="banner_1">
         <div class="slideshow-text text-white">
             <div>Early detection of Hearing Impairment Helps</div>
         </div>
     </div>
     <div class="slideshow-item">
-        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/josh_home_banner_2.jpg" alt="banner_2">
+        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/josh_home_banner_2.jpg" alt="banner_2">
         <div class="slideshow-text text-white">
             <div>Provided Hearing Aid to 1000+ Children</div>
         </div>
     </div>
     <div class="slideshow-item">
-        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/10/hearing-aid.jpg" alt="summer">
+        <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/11/hearing-aid.jpg" alt="summer">
         <div class="slideshow-text text-white">
             <div>Helping Hearing Impaired for a Normal Life</div>
         </div>
